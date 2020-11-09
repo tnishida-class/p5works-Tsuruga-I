834b89b6f56e91e48eb91e0f2c34c18631bb966b
Tsuruga-I <itsuki2817@gmail.com>
2020-11-09 15:56:17 +0900

    Change section3-3, 3-q1

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -4,15 +4,15 @@
 function setup(){
   createCanvas(400, 400);
   background(255);
-  balloon("I love keyakizaka46");
+  balloon("I love keyakizaka46", 30,50);
 }
 
-function balloon(t){
+function balloon(t, x, y){
   let w = textWidth(t);
   let h = textAscent() + textDescent();
-  let p = 2;
+  let p = 6;
   fill(0);
-  rect(0, 0, w + p * 2, h + p * 2);
+  rect(x, y, w + p * 2, h + p * 2);
   fill(255);
-  text(t, p, h + p);
+  text(t, x + p, y + h + p/3);
 }

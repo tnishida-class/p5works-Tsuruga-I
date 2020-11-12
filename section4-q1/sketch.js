0798e2cd8275fbedec3b33b0f9a31117b6501b7c
Tsuruga-I <itsuki2817@gmail.com>
2020-11-12 16:26:32 +0900

    Change 4-q1

--- section4-q1/sketch.js
+++ section4-q1/sketch.js
@@ -12,5 +12,19 @@ function draw(){
   background(160, 192, 255);
   count = (count + 1) % cycle;
   // BLANK[1]
+  //for(count == 0; count < 100; count++){
+    if( count == 0){
+      size == size +10
+    }
+    else{
+      size == size -10
+  }
+
+//  }
+//fill(255);
   ellipse(width / 2, height / 2, size);
+
+  //size += 2
+  //if (size > width){size -= 2}
+  //else if (size < 0){size += 2}
 }

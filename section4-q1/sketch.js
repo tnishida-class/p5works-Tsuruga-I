c3449d5390754d30dcae9e372728f83a597bcdf8
Tsuruga-I <itsuki2817@gmail.com>
2020-11-16 17:41:48 +0900

    Changes section4-q1, q2

--- section4-q1/sketch.js
+++ section4-q1/sketch.js
@@ -1,30 +1,27 @@
 // 練習問題「心臓の鼓動のように大きくなったり小さくなったりする円」
 let count;
 let cycle;
+let size;
 
 function setup(){
   createCanvas(200, 200);
   count = 0;
   cycle = 100;
+  size = 20;
 }
 
 function draw(){
   background(160, 192, 255);
   count = (count + 1) % cycle;
   // BLANK[1]
-  //for(count == 0; count < 100; count++){
-    if( count == 0){
-      size == size +10
-    }
-    else{
-      size == size -10
+  if(count < 50){
+    size += 1;
+  }
+  else{
+    size -= 1;
   }
 
-//  }
-//fill(255);
+ fill(94,185,84);
   ellipse(width / 2, height / 2, size);
 
-  //size += 2
-  //if (size > width){size -= 2}
-  //else if (size < 0){size += 2}
-}
+    }
